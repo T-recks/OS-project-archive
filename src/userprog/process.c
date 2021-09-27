a7ebf0709a3bc56904e416345b2428ff3fea87e4
Dante Bourret <danteb77@berkeley.edu>
2021-09-27 00:34:01 +0000

    forgot to commit this file

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -470,8 +470,9 @@ static bool setup_stack(void** esp) {
   kpage = palloc_get_page(PAL_USER | PAL_ZERO);
   if (kpage != NULL) {
     success = install_page(((uint8_t*)PHYS_BASE) - PGSIZE, kpage, true);
-    if (success)
-      *esp = PHYS_BASE;
+    if (success) {
+      *esp = PHYS_BASE - 20;
+    }
     else
       palloc_free_page(kpage);
   }
