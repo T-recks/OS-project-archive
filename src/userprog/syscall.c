b3e1dc7e6225517aff35ddb1579fa0b58e139cda
Averylevin <averylevin23@gmail.com>
2021-11-05 17:51:39 +0000

    bones of user threads

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -33,7 +33,7 @@ void close_all_files(void) {
   }
   lock_acquire(&filesys_lock);
   while (!list_empty(fd_table)) {
-    struct list_elem *e = list_pop_front(fd_table);
+    struct list_elem* e = list_pop_front(fd_table);
     struct file_data* f = list_entry(e, struct file_data, elem);
     file_close(f->file);
     f->ref_cnt--;
@@ -50,19 +50,17 @@ void clear_cmdline(void) {
   if (argv == NULL)
     return;
   while (!list_empty(argv)) {
-    struct list_elem *e = list_pop_front(argv);
+    struct list_elem* e = list_pop_front(argv);
     struct word* w = list_entry(e, struct word, elem);
     list_remove(e);
     free(w);
   }
 }
 
-static int handle_practice(int val) {
-  return val + 1;
-}
+static int handle_practice(int val) { return val + 1; }
 
 void handle_exit(int status) {
-  struct process *pcb = thread_current()->pcb;
+  struct process* pcb = thread_current()->pcb;
   printf("%s: exit(%d)\n", pcb->process_name, status);
   if (pcb->ws == NULL) {
     goto done;
@@ -83,9 +81,9 @@ void handle_exit(int status) {
     goto done;
   }
   // Decrement the ref count of each of the child waiters
-  struct list_elem *e;
-  struct wait_status *w;
-  for (e = list_begin(pcb->waits); e!= list_end(pcb->waits); e = list_next(e)) {
+  struct list_elem* e;
+  struct wait_status* w;
+  for (e = list_begin(pcb->waits); e != list_end(pcb->waits); e = list_next(e)) {
     w = list_entry(e, struct wait_status, elem);
     lock_acquire(&w->lock);
     w->ref_cnt -= 1;
@@ -101,16 +99,16 @@ done:
   process_exit();
 }
 
-static int handle_exec(const char *cmd_line) {
-  
+static int handle_exec(const char* cmd_line) {
+
   // Initialize the share wait status struct
-  struct wait_status *ws = (struct wait_status*)malloc(sizeof(struct wait_status));
+  struct wait_status* ws = (struct wait_status*)malloc(sizeof(struct wait_status));
   sema_init(&ws->sema_load, 0);
   sema_init(&ws->sema_wait, 0);
   lock_init(&ws->lock);
   ws->loaded = false;
   ws->ref_cnt = 2;
-  
+
   pid_t pid = process_execute(cmd_line, ws);
   // Wait for the child process to finish loading
   sema_down(&ws->sema_load); // Child calls sema_up in start_process
@@ -119,12 +117,12 @@ static int handle_exec(const char *cmd_line) {
     return -1;
   } else {
     // Add the child to the list of active children
-    struct list_elem *e = (struct list_elem*)malloc(sizeof(struct list_elem));
+    struct list_elem* e = (struct list_elem*)malloc(sizeof(struct list_elem));
     ws->elem = *e;
     ws->loaded = true;
     ws->pid = pid;
     list_push_back(thread_current()->pcb->waits, &ws->elem);
-    
+
     return pid;
   }
 }
@@ -184,7 +182,7 @@ static int handle_filesize(int fd) {
   struct list* fd_table = thread_current()->pcb->open_files;
   lock_acquire(&filesys_lock);
   // check the fd table for the given fd, return false if not present
-  struct file_data *f = find_file(fd, fd_table);
+  struct file_data* f = find_file(fd, fd_table);
   if (f != NULL) {
     int length = file_length(f->file);
     lock_release(&filesys_lock);
@@ -198,7 +196,7 @@ static int handle_read(int fd, void* buffer, unsigned size) {
   struct list* fd_table = thread_current()->pcb->open_files;
   lock_acquire(&filesys_lock);
   // check the fd table for the given fd, return false if not present
-  struct file_data *f = find_file(fd, fd_table);
+  struct file_data* f = find_file(fd, fd_table);
   if (f != NULL) {
     int result = file_read(f->file, buffer, size);
     lock_release(&filesys_lock);
@@ -238,7 +236,7 @@ static int handle_write(uint32_t* args) {
     return size;
   } else {
     struct list* fd_table = thread_current()->pcb->open_files;
-    struct file_data *f = find_file(fd, fd_table);
+    struct file_data* f = find_file(fd, fd_table);
     if (f != NULL) {
       int result = file_write(f->file, buf, size);
       lock_release(&filesys_lock);
@@ -252,7 +250,7 @@ static int handle_write(uint32_t* args) {
 static void handle_seek(int fd, unsigned position) {
   lock_acquire(&filesys_lock);
   struct list* fd_table = thread_current()->pcb->open_files;
-  struct file_data *f = find_file(fd, fd_table);
+  struct file_data* f = find_file(fd, fd_table);
   if (f != NULL) {
     file_seek(f->file, position);
   }
@@ -262,7 +260,7 @@ static void handle_seek(int fd, unsigned position) {
 static unsigned handle_tell(int fd) {
   lock_acquire(&filesys_lock);
   struct list* fd_table = thread_current()->pcb->open_files;
-  struct file_data *f = find_file(fd, fd_table);
+  struct file_data* f = find_file(fd, fd_table);
   if (f != NULL) {
     int position = file_tell(f->file);
     lock_release(&filesys_lock);
@@ -272,8 +270,18 @@ static unsigned handle_tell(int fd) {
   return -1;
 }
 
-static int handle_compute_e(int n) {
-  return sys_sum_to_e(n);
+static int handle_compute_e(int n) { return sys_sum_to_e(n); }
+
+static tid_t handle_pthread_create() {
+  // TODO: create thread
+}
+
+static void handle_pthread_exit() {
+  // TODO: exit thread
+}
+
+static void handle_pthread_join() {
+  // TODO: join thread
 }
 
 /* Validate ARGS by ensuring each address points to valid memory.
@@ -282,9 +290,10 @@ static int handle_compute_e(int n) {
 */
 static void validate_args(struct intr_frame* f, uint32_t* args, int n) {
   int i = 1;
-  
+
   int vldt_i = -1;
-  if (args[0] == SYS_EXEC || args[0] == SYS_OPEN || args[0] == SYS_CREATE || args[0] == SYS_REMOVE) {
+  if (args[0] == SYS_EXEC || args[0] == SYS_OPEN || args[0] == SYS_CREATE ||
+      args[0] == SYS_REMOVE) {
     vldt_i = 1;
   }
   if (args[0] == SYS_READ || args[0] == SYS_WRITE) {
@@ -292,35 +301,40 @@ static void validate_args(struct intr_frame* f, uint32_t* args, int n) {
   }
   if (vldt_i != -1) {
     // Argument is a pointer, make sure it's valid
-    if (is_kernel_vaddr((void*)(&args[vldt_i]+1)) || pagedir_get_page(active_pd(), (void*)&args[vldt_i]+1) == NULL || args[vldt_i] == (int)NULL) {
+    if (is_kernel_vaddr((void*)(&args[vldt_i] + 1)) ||
+        pagedir_get_page(active_pd(), (void*)&args[vldt_i] + 1) == NULL ||
+        args[vldt_i] == (int)NULL) {
       f->eax = -1;
       handle_exit(-1);
     }
-    if (is_kernel_vaddr((void*)(args[vldt_i]+1)) || pagedir_get_page(active_pd(), (void*)args[vldt_i]+1) == NULL || args[vldt_i] == (int)NULL) {
+    if (is_kernel_vaddr((void*)(args[vldt_i] + 1)) ||
+        pagedir_get_page(active_pd(), (void*)args[vldt_i] + 1) == NULL ||
+        args[vldt_i] == (int)NULL) {
       f->eax = -1;
       handle_exit(-1);
     }
   }
-  
-  for (; i != n+1; i++) {
-    if ((void*)args[i] == NULL && (args[0] != SYS_EXIT && args[0] != SYS_READ && args[0] != SYS_WRITE &&
-                                   args[0] != SYS_SEEK && args[0] != SYS_TELL && args[0] != SYS_CREATE)) {
+
+  for (; i != n + 1; i++) {
+    if ((void*)args[i] == NULL &&
+        (args[0] != SYS_EXIT && args[0] != SYS_READ && args[0] != SYS_WRITE &&
+         args[0] != SYS_SEEK && args[0] != SYS_TELL && args[0] != SYS_CREATE)) {
       // exit(0) is a successfull exit, reading/writing 0 bytes is valid
       break;
     }
     // For checking if the pointer is to invalid memory, add 1 byte to the
     // address to account for the case some bytes of the address are
     // valid but the others are not (address lies on a page boundary).
-    if (is_kernel_vaddr((void*)(&args[i]+1))) {
+    if (is_kernel_vaddr((void*)(&args[i] + 1))) {
       // Referencing kernel memory.
       break;
     }
-    if (pagedir_get_page(active_pd(), (void*)(&args[i]+1)) == NULL) {
+    if (pagedir_get_page(active_pd(), (void*)(&args[i] + 1)) == NULL) {
       // Referencing memory not in the page directory
       break;
     }
   }
-  if (i != n+1) {
+  if (i != n + 1) {
     // Invalid memory access, terminate the process
     f->eax = -1;
     handle_exit(-1);
@@ -337,10 +351,11 @@ static void syscall_handler(struct intr_frame* f UNUSED) {
    * include it in your final submission.
    */
 
-//  printf("System call number: %d\n", args[0]);
-  
+  //  printf("System call number: %d\n", args[0]);
+
   // Case where the stack is too large and all bytes of the esp are in invalid memory
-  if (f->ebp - (uint32_t)f->esp > 4096 && pagedir_get_page(active_pd(), (void*)(f->esp+1)) == NULL) {
+  if (f->ebp - (uint32_t)f->esp > 4096 &&
+      pagedir_get_page(active_pd(), (void*)(f->esp + 1)) == NULL) {
     handle_exit(-1);
   }
 
@@ -404,6 +419,18 @@ static void syscall_handler(struct intr_frame* f UNUSED) {
     case SYS_COMPUTE_E:
       //TODO: Validate
       f->eax = handle_compute_e(args[1]);
+      break;
+    case SYS_PT_CREATE:
+      //TODO: Validate
+      f->eax = handle_pthread_create();
+      break;
+    case SYS_PT_EXIT:
+      //TODO: Validate
+      handle_pthread_exit();
+      break;
+    case SYS_PT_JOIN:
+      //TODO: Validate
+      handle_pthread_join();
       break;
   }
 }
