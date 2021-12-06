0b933dc486589357915b3e9f2daa05c5c5bafe75
Timothy Recker <timjrecker@gmail.com>
2021-12-06 12:46:34 -0800

    moving struct dir definitions to directory.h for inclusion

--- src/filesys/directory.c
+++ src/filesys/directory.c
@@ -6,21 +6,6 @@
 #include "filesys/inode.h"
 #include "threads/malloc.h"
 
-/* A directory. */
-struct dir {
-  struct inode* inode; /* Backing store. */
-  off_t pos;           /* Current position. */
-};
-
-/* A single directory entry. */
-struct dir_entry {
-  block_sector_t inode_sector; /* Sector number of header. */
-  char name[NAME_MAX + 1];     /* Null terminated file name. */
-  bool in_use;                 /* In use or free? */
-  struct dir_entry* loc;       /* Pointer to "." directory */
-  struct dir_entry* parent;    /* Pointer to ".." directory */
-};
-
 /* Creates a directory with space for ENTRY_CNT entries in the
    given SECTOR.  Returns true if successful, false on failure. */
 bool dir_create(block_sector_t sector, size_t entry_cnt) {
